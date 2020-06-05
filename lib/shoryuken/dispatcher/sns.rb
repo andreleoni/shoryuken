5cd4e9231591b7076124a3e57856526d831fbfb8
André Leoni <andreluizleoni@gmail.com>
2020-06-05 09:30:08 -0300

    Created shared example to shoryuken worker

--- lib/shoryuken/dispatcher/sns.rb
+++ lib/shoryuken/dispatcher/sns.rb
@@ -2,7 +2,7 @@ module Shoryuken::Dispatcher
   module Sns
     def self.included(base)
       base.include(Shoryuken::Worker)
-      # base.extend(ClassMethods)
+      base.extend(ClassMethods)
     end
 
     module ClassMethods
