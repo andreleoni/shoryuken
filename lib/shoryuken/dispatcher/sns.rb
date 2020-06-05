d9dbc3f74f45c143af1037b459b4a3b92b056f38
André Leoni <andreluizleoni@gmail.com>
2020-06-05 13:49:57 -0300

    c

--- lib/shoryuken/dispatcher/sns.rb
+++ lib/shoryuken/dispatcher/sns.rb
@@ -6,6 +6,23 @@ module Shoryuken::Dispatcher
     end
 
     module ClassMethods
+      def perform(sns_params)
+        # key_value = sns_params.dig(:MessageAttributes, dispacher_options[:key], :Value) # service
+        # dispacher_config = dispachers[key_value]
+        # worker_params = sns_params[:subset]
+
+        # call_dispacher(dispacher_config, worker_params) if dispacher_config
+      end
+
+      private
+
+      def call_dispacher(dispacher_config, worker_params)
+        # if dispacher_config[:async]
+        #   dispacher_config[:to].perform_async(worker_params)
+        # else
+        #   dispacher_config[:to].new.perform(worker_params)
+        # end
+      end
     end
   end
 end
--- spec/shoryuken/dispatcher/sns_spec.rb
+++ spec/shoryuken/dispatcher/sns_spec.rb
@@ -3,4 +3,24 @@ require 'shared_examples_for_shoryuken_worker'
 
 RSpec.describe Shoryuken::Dispatcher::Sns do
   it_behaves_like 'shoryuken_worker'
+
+  describe '.shoryuken_dispatcher_options' do
+    it 'registers a worker' do
+      expect(Shoryuken.worker_registry.workers('default')).to eq([TestWorker])
+    end
+
+    it 'accepts a block as a queue' do
+      eval("class NewTestWorker
+        include #{described_class}
+
+        shoryuken_dispatcher_options queue: :messages, key: :testkey
+
+        dispach 'create', to: ShipmentCreateWorker, async: true
+        dispach 'destroy', to: ShipmentDestroyWorker, async: false
+      end")
+
+      expect(Shoryuken.worker_registry.workers('production_default')).to eq([NewTestWorker])
+      expect(NewTestWorker.get_shoryuken_options['queue']).to eq 'production_default'
+    end
+  end
 end
